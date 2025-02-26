c0431803afb83f0bcf2731ca00cde6af390f5b1b
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-02-26 14:40:00 +0100

    Add: Chiamata avvenuta con successo

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+
+  const [posts, setPosts] = useState([]);
+  const fetchPosts = () => {
+    fetch("http://localhost:3000/posts")
+      .then(response => response.json())
+      .then(data => setPosts(data));
+
+  };
+  useEffect(fetchPosts, []);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>lista dei posts</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>TITOLO</th>
+              <th>CONTENUTO</th>
+              <th>IMAGINE</th>
+            </tr>
+          </thead>
+          <tbody>
+            {posts.map(post => (
+              <tr key={post.id}>
+                <td>{post.id}</td>
+                <td>{post.title}</td>
+                <td>{post.content}</td>
+                <td>{post.image}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+
+
